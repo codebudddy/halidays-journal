9e90e13496daeb46ce96d1cd07e1c20aceb5e24d
bitdoctrine <bitdoctrine@gmail.com>
2022-12-03 07:36:01 +0100

    Added and implimented date filters for posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { trim } from '../helpers';
 
 import './posts.css';
@@ -7,13 +7,14 @@ import { useCollection } from '../hooks/useCollections';
 import { formatDistance } from 'date-fns';
 
 const Posts = () => {
-  const navigate = useNavigate();
-  const { document, error } = useCollection('entries');
+  const { document } = useCollection('entries');
   const [datesArray, setDatesArray] = useState([]);
+  const [date, setDate] = useState('All');
+  const [docs, setDocs] = useState([]);
 
   useEffect(() => {
     const fetchDates = () => {
-      let dates = [];
+      let dates = ['All'];
       document?.forEach((d) => {
         if (!dates.includes(d?.createdAt?.toDate().toLocaleDateString())) {
           dates.push(d.createdAt?.toDate().toLocaleDateString());
@@ -21,15 +22,30 @@ const Posts = () => {
       });
       setDatesArray(dates);
     };
+
+    const setFilters = () => {
+      if (date === 'All' && document) {
+        setDocs(document);
+      } else {
+        if (document) {
+          const newFilter = document.filter(
+            (d) => d.createdAt.toDate().toLocaleDateString() === date
+          );
+          setDocs(newFilter);
+        }
+      }
+    };
+
     fetchDates();
-  }, [document]);
+    setFilters();
+  }, [document, date]);
   return (
     <div className="posts">
       <div className="posts__by-dates">
         {datesArray &&
           datesArray.map((element) => (
             <div
-              onClick={() => navigate(`/dates/${element}`)}
+              onClick={() => setDate(element)}
               className="posts__by-dates_date"
               key={element}
             >
@@ -38,8 +54,8 @@ const Posts = () => {
           ))}
       </div>
       <div className="posts__entries">
-        {document &&
-          document.map((entry) => (
+        {docs &&
+          docs.map((entry) => (
             <div className="posts__entries-entry" key={entry.id}>
               <i className="posts__entries-entry_id">{trim(entry.id, 5)}</i>
               <h3>
